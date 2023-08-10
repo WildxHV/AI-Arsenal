f976da5436bfc6d5cb5f3baeeec5f1310f568d6c
WildxHV <hvsisodia02@gmail.com>
2023-08-10 13:54:58 +0530

    added createsuperuser command

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
